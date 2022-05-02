f17407fe860f61ba0a690907ae19a169911cf173
GodKelvin <kelvin.lehrback@gmail.com>
2022-05-02 14:40:55 -0300

    definindo funcao de posicao

--- main.py
+++ main.py
@@ -5,6 +5,11 @@ import numpy as np
 def fitness_function(position):
     return position[0]**2 + position[1]**2 + 1
 
+
+def random_position():
+    #Intervalos designados na especificacao
+    return [random.uniform(-512, 512),random.uniform(-512, 512)] 
+    
 #Some variables to calculate the velocity
 W = 0.5
 c1 = 0.5
@@ -18,43 +23,44 @@ n_iterations = 100
 # target_error = float(input("Inform the target error: "))
 
 #1 - Determinar o numero de particulas P da populacao
-n_particles = 30
+qtd_particulas = 30
 
 #2 - Inicializar aleatoriamente a posicao inicial(x) de cada particula
 #MELHORAR ISSO AQUI
-particle_position_vector = np.array([np.array([(-1) ** (bool(random.getrandbits(1))) * random.random()*50, (-1)**(bool(random.getrandbits(1))) * random.random()*50]) for _ in range(n_particles)])
+particulas = np.array([np.array(random_position()) for _ in range(qtd_particulas)])
 
-pbest_position = particle_position_vector
-pbest_fitness_value = np.array([float('inf') for _ in range(n_particles)])
+pbest_position = particulas
+pbest_fitness_value = np.array([float('inf') for _ in range(qtd_particulas)])
 gbest_fitness_value = float('inf')
 gbest_position = np.array([float('inf'), float('inf')])
 
 #3 - Velocidade inicial(v) para todas as particulas
-velocity_vector = ([np.array([0, 0]) for _ in range(n_particles)])
+#Atribuir numero aleatorio
+velocity_vector = ([np.array([0, 0]) for _ in range(qtd_particulas)])
 iteration = 0
 
 
 while iteration < n_iterations:
-    for i in range(n_particles):
+    for i in range(qtd_particulas):
         #4 a) - Calculando aptidao de 'p'
-        fitness_cadidate = fitness_function(particle_position_vector[i])
-        #print(fitness_cadidate, ' ', particle_position_vector[i])
+        fitness_cadidate = fitness_function(particulas[i])
+        #print(fitness_cadidate, ' ', particulas[i])
         
         #4 b) - Verificando a melhor posicao de 'p'
         if(pbest_fitness_value[i] > fitness_cadidate):
             pbest_fitness_value[i] = fitness_cadidate
-            pbest_position[i] = particle_position_vector[i]
+            pbest_position[i] = particulas[i]
 
         #5 - Verificando a melhor aptdao da populacao
         if(gbest_fitness_value > fitness_cadidate):
             gbest_fitness_value = fitness_cadidate
-            gbest_position = particle_position_vector[i]
+            gbest_position = particulas[i]
     
-    for i in range(n_particles):
+    for i in range(qtd_particulas):
         #6 a) - Atualizando velocidade
-        new_velocity = (W*velocity_vector[i]) + (c1*random.random()) * (pbest_position[i] - particle_position_vector[i]) + (c2*random.random()) * (gbest_position-particle_position_vector[i])
-        new_position = new_velocity + particle_position_vector[i]
-        particle_position_vector[i] = new_position
+        new_velocity = (W*velocity_vector[i]) + (c1*random.random()) * (pbest_position[i] - particulas[i]) + (c2*random.random()) * (gbest_position-particulas[i])
+        new_position = new_velocity + particulas[i]
+        particulas[i] = new_position
 
     iteration = iteration + 1
     
