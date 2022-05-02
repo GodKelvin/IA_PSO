f67f477f2c8571341b31e5ca47809f2aff09081b
GodKelvin <kelvin.lehrback@gmail.com>
2022-05-02 15:24:27 -0300

    wip

--- main.py
+++ main.py
@@ -7,8 +7,12 @@ def fitness_function(position):
 
 
 def random_position():
-    #Intervalos designados na especificacao
+    #Intervalos designados na especificacao do trabalho
     return [random.uniform(-512, 512),random.uniform(-512, 512)] 
+
+def random_velocidade():
+    #Intervalos designados na especificacao do trabalho
+    return [random.uniform(-77, 77),random.uniform(-77, 77)]
     
 #Some variables to calculate the velocity
 W = 0.5
@@ -26,7 +30,6 @@ n_iterations = 100
 qtd_particulas = 30
 
 #2 - Inicializar aleatoriamente a posicao inicial(x) de cada particula
-#MELHORAR ISSO AQUI
 particulas = np.array([np.array(random_position()) for _ in range(qtd_particulas)])
 
 pbest_position = particulas
@@ -35,16 +38,17 @@ gbest_fitness_value = float('inf')
 gbest_position = np.array([float('inf'), float('inf')])
 
 #3 - Velocidade inicial(v) para todas as particulas
-#Atribuir numero aleatorio
-velocity_vector = ([np.array([0, 0]) for _ in range(qtd_particulas)])
+#AJUSTAR ISSO AQUI / VERIFICAR
+velocidade_inicial = random_velocidade()
+vetor_velocidade = ([np.array(velocidade_inicial) for _ in range(qtd_particulas)])
+#vetor_velocidade = ([np.array([0, 0]) for _ in range(qtd_particulas)])
+
+#RENOMEAR ITERATION
 iteration = 0
-
-
 while iteration < n_iterations:
     for i in range(qtd_particulas):
         #4 a) - Calculando aptidao de 'p'
         fitness_cadidate = fitness_function(particulas[i])
-        #print(fitness_cadidate, ' ', particulas[i])
         
         #4 b) - Verificando a melhor posicao de 'p'
         if(pbest_fitness_value[i] > fitness_cadidate):
@@ -58,7 +62,7 @@ while iteration < n_iterations:
     
     for i in range(qtd_particulas):
         #6 a) - Atualizando velocidade
-        new_velocity = (W*velocity_vector[i]) + (c1*random.random()) * (pbest_position[i] - particulas[i]) + (c2*random.random()) * (gbest_position-particulas[i])
+        new_velocity = (W*vetor_velocidade[i]) + (c1*random.random()) * (pbest_position[i] - particulas[i]) + (c2*random.random()) * (gbest_position-particulas[i])
         new_position = new_velocity + particulas[i]
         particulas[i] = new_position
 
